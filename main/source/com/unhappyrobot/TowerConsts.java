78851c7a30b4c1edaa4d26c9ebd0ef8637b2fec9
Phil Plante <unhappyrobot@gmail.com>
2012-03-28 23:47:08 -0700

    Artifacts from release-v0.10.38

--- main/source/com/unhappyrobot/TowerConsts.java
+++ main/source/com/unhappyrobot/TowerConsts.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 
 public class TowerConsts {
-  public static boolean DEBUG = true;
-  public static String VERSION = "v0.10.37";
-  public static String GIT_SHA = "9a555b9";
-  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
+  public static boolean DEBUG = false;
+  public static String VERSION = "v0.10.38";
+  public static String GIT_SHA = "0950399";
+  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
   public static final String HAPPYDROIDS_URI = "http://" + HAPPYDROIDS_SERVER;
 
   public static boolean ENABLE_HAPPYDROIDS_CONNECT = false;
