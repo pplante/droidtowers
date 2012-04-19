fe00b5b746157b5a554938a6887302e0ab7fb351
Phil Plante <unhappyrobot@gmail.com>
2012-04-19 03:09:59 -0700

    fuck proguard so hard

--- happydroids-common/src/com/happydroids/HappyDroidConsts.java
+++ happydroids-common/src/com/happydroids/HappyDroidConsts.java
@@ -5,8 +5,8 @@
 package com.happydroids;
 
 public class HappyDroidConsts {
-  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
-  public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
+  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
+  public static final String HAPPYDROIDS_URI = "http://" + HAPPYDROIDS_SERVER;
   public static boolean DEBUG = true;
   public static String VERSION = "v0.30.1";
   public static String GIT_SHA = "2b3dd23";
--- main/source/com/happydroids/droidtowers/TowerAssetManagerFilesList.java
+++ main/source/com/happydroids/droidtowers/TowerAssetManagerFilesList.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2012. HappyDroids LLC, All rights reserved.
- */
+* Copyright (c) 2012. HappyDroids LLC, All rights reserved.
+*/
 
 package com.happydroids.droidtowers;
 
@@ -14,53 +14,53 @@ import com.google.common.collect.Maps;
 import java.util.Map;
 
 public class TowerAssetManagerFilesList {
-  public static Map<String, Class> preloadFiles = Maps.newHashMap();
-  public static Map<String, Class> files = Maps.newHashMap();
+public static Map<String, Class> preloadFiles = Maps.newHashMap();
+public static Map<String, Class> files = Maps.newHashMap();
 
-  static {
-    files.put("fonts/menlo_14_bold_white.fnt", BitmapFont.class);
-    files.put("hud/menus.txt", TextureAtlas.class);
-    files.put("fonts/bank_gothic_32.fnt", BitmapFont.class);
-    files.put("fonts/helvetica_neue_12_white.fnt", BitmapFont.class);
-    files.put("hud/buttons.txt", TextureAtlas.class);
-    files.put("sound/effects/click.wav", Sound.class);
-    files.put("decals.png", Texture.class);
-    files.put("sound/effects/construction-destroy-1.wav", Sound.class);
-    files.put("backgrounds/ground-top.png", Texture.class);
-    files.put("movies/avengers.txt", TextureAtlas.class);
-    preloadFiles.put("backgrounds/clouds.txt", TextureAtlas.class);
-    files.put("sound/effects/construction-placement-1.wav", Sound.class);
-    files.put("hud/misc.txt", TextureAtlas.class);
-    files.put("fonts/roboto_white_28.fnt", BitmapFont.class);
-    files.put("transport.txt", TextureAtlas.class);
-    files.put("fonts/roboto_white_64.fnt", BitmapFont.class);
-    files.put("rooms/housing.txt", TextureAtlas.class);
-    files.put("rooms/generic.txt", TextureAtlas.class);
-    files.put("characters.txt", TextureAtlas.class);
-    files.put("hud/test.txt", TextureAtlas.class);
-    files.put("fonts/roboto_white_32.fnt", BitmapFont.class);
-    files.put("hud/buttons-hd.txt", TextureAtlas.class);
-    files.put("fonts/roboto_white_48.fnt", BitmapFont.class);
-    files.put("fonts/bank_gothic_64.fnt", BitmapFont.class);
-    files.put("fonts/roboto_white_12.fnt", BitmapFont.class);
-    files.put("fonts/roboto_white_128.fnt", BitmapFont.class);
-    preloadFiles.put("default-skin.ui", Skin.class);
-    files.put("fonts/menlo_16.fnt", BitmapFont.class);
-    files.put("backgrounds/cityscape.txt", TextureAtlas.class);
-    preloadFiles.put("hud/menus.txt", TextureAtlas.class);
-    files.put("fonts/roboto_bold_white_18.fnt", BitmapFont.class);
-    files.put("rain-drop.png", Texture.class);
-    files.put("backgrounds/cityscape.png", Texture.class);
-    files.put("fonts/roboto_white_14.fnt", BitmapFont.class);
-    files.put("backgrounds/clouds.txt", TextureAtlas.class);
-    files.put("fonts/helvetica_neue_18.fnt", BitmapFont.class);
-    files.put("backgrounds/ground-tile.png", Texture.class);
-    files.put("backgrounds/clouds.png", Texture.class);
-    files.put("fonts/helvetica_neue_14_black.fnt", BitmapFont.class);
-    files.put("fonts/roboto_white_24.fnt", BitmapFont.class);
-    files.put("fonts/hiragino_maru_14_white.fnt", BitmapFont.class);
-    files.put("fonts/helvetica_neue_10_bold_white.fnt", BitmapFont.class);
-    files.put("backgrounds/sky-gradient.png", Texture.class);
-    files.put("fonts/helvetica_neue_18_white.fnt", BitmapFont.class);
-  }
+static {
+files.put("fonts/menlo_14_bold_white.fnt", BitmapFont.class);
+files.put("hud/menus.txt", TextureAtlas.class);
+files.put("fonts/bank_gothic_32.fnt", BitmapFont.class);
+files.put("fonts/helvetica_neue_12_white.fnt", BitmapFont.class);
+files.put("hud/buttons.txt", TextureAtlas.class);
+files.put("sound/effects/click.wav", Sound.class);
+files.put("decals.png", Texture.class);
+files.put("sound/effects/construction-destroy-1.wav", Sound.class);
+files.put("backgrounds/ground-top.png", Texture.class);
+files.put("movies/avengers.txt", TextureAtlas.class);
+preloadFiles.put("backgrounds/clouds.txt", TextureAtlas.class);
+files.put("sound/effects/construction-placement-1.wav", Sound.class);
+files.put("hud/misc.txt", TextureAtlas.class);
+files.put("fonts/roboto_white_28.fnt", BitmapFont.class);
+files.put("transport.txt", TextureAtlas.class);
+files.put("fonts/roboto_white_64.fnt", BitmapFont.class);
+files.put("rooms/housing.txt", TextureAtlas.class);
+files.put("rooms/generic.txt", TextureAtlas.class);
+files.put("characters.txt", TextureAtlas.class);
+files.put("hud/test.txt", TextureAtlas.class);
+files.put("fonts/roboto_white_32.fnt", BitmapFont.class);
+files.put("hud/buttons-hd.txt", TextureAtlas.class);
+files.put("fonts/roboto_white_48.fnt", BitmapFont.class);
+files.put("fonts/bank_gothic_64.fnt", BitmapFont.class);
+files.put("fonts/roboto_white_12.fnt", BitmapFont.class);
+files.put("fonts/roboto_white_128.fnt", BitmapFont.class);
+preloadFiles.put("default-skin.ui", Skin.class);
+files.put("fonts/menlo_16.fnt", BitmapFont.class);
+files.put("backgrounds/cityscape.txt", TextureAtlas.class);
+preloadFiles.put("hud/menus.txt", TextureAtlas.class);
+files.put("fonts/roboto_bold_white_18.fnt", BitmapFont.class);
+files.put("rain-drop.png", Texture.class);
+files.put("backgrounds/cityscape.png", Texture.class);
+files.put("fonts/roboto_white_14.fnt", BitmapFont.class);
+files.put("backgrounds/clouds.txt", TextureAtlas.class);
+files.put("fonts/helvetica_neue_18.fnt", BitmapFont.class);
+files.put("backgrounds/ground-tile.png", Texture.class);
+files.put("backgrounds/clouds.png", Texture.class);
+files.put("fonts/helvetica_neue_14_black.fnt", BitmapFont.class);
+files.put("fonts/roboto_white_24.fnt", BitmapFont.class);
+files.put("fonts/hiragino_maru_14_white.fnt", BitmapFont.class);
+files.put("fonts/helvetica_neue_10_bold_white.fnt", BitmapFont.class);
+files.put("backgrounds/sky-gradient.png", Texture.class);
+files.put("fonts/helvetica_neue_18_white.fnt", BitmapFont.class);
+}
 }
