7728349238ecfa3407a50349e6684308dd5eee3f
Phil Plante <unhappyrobot@gmail.com>
2012-04-19 03:10:53 -0700

    fuck proguard more

--- happydroids-common/src/com/happydroids/HappyDroidConsts.java
+++ happydroids-common/src/com/happydroids/HappyDroidConsts.java
@@ -5,8 +5,8 @@
 package com.happydroids;
 
 public class HappyDroidConsts {
-  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
-  public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
+  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
+  public static final String HAPPYDROIDS_URI = "http://" + HAPPYDROIDS_SERVER;
   public static boolean DEBUG = true;
   public static String VERSION = "v0.30.2";
   public static String GIT_SHA = "fe00b5b";
