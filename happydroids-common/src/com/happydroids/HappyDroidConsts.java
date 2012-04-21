2924f006dc11a529e4969ec4399f0e4e5adf176a
Phil Plante <unhappyrobot@gmail.com>
2012-04-21 01:43:45 -0700

    Artifacts from release-v0.40.3

--- happydroids-common/src/com/happydroids/HappyDroidConsts.java
+++ happydroids-common/src/com/happydroids/HappyDroidConsts.java
@@ -8,6 +8,6 @@ public class HappyDroidConsts {
   public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
   public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
   public static final boolean DEBUG = false;
-  public static String VERSION = "v0.40.2";
-  public static String GIT_SHA = "c24b0e5f96368418756cdcc5b028da4cfd195769";
+  public static String VERSION = "v0.40.3";
+  public static String GIT_SHA = "f712eba55a2c1b5d682b429f140a2ade3d00954e";
 }
