ae52d68afbfe1ba0f5f1cd5a62a0ac0b9dba782b
Phil Plante <unhappyrobot@gmail.com>
2012-06-25 01:09:33 +0200

    added ability to go back in the web dialog

--- android/source/com/happydroids/droidtowers/platform/TowerWebBrowser.java
+++ android/source/com/happydroids/droidtowers/platform/TowerWebBrowser.java
@@ -10,6 +10,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -96,5 +97,17 @@ public class TowerWebBrowser extends Dialog {
     if (uriToLoad != null) {
       webView.loadUrl(uriToLoad);
     }
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    // Check if the key event was the Back button and if there's history
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+      webView.goBack();
+      return true;
+    }
+    // If it wasn't the Back key or there's no web page history, bubble up to the default
+    // system behavior (probably exit the activity)
+    return super.onKeyDown(keyCode, event);
   }
 }
