63be9e1a59f8fdcfedf72aa35973630bf7f7001f
Phil Plante <unhappyrobot@gmail.com>
2012-06-27 02:32:49 +0200

    moved the screenshot code

---
+++ main/source/com/happydroids/droidtowers/utils/Screenshot.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012. HappyDroids LLC, All rights reserved.
+ */
+
+package com.happydroids.droidtowers.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import static com.badlogic.gdx.Application.ApplicationType.Android;
+
+public class ScreenShot {
+  public static void capture() {
+    Pixmap pixmap = captureFromBuffer(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+
+    try {
+      String fileName = "DroidTowers_" + System.currentTimeMillis() + ".png";
+      FileHandle storagePath = Gdx.files.external(Gdx.app.getType().equals(Android) ? "" : "Desktop/");
+      FileHandle screenShotFile = storagePath.child(fileName);
+      screenShotFile.writeBytes(PNG.toPNG(pixmap), false);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static Pixmap captureFromBuffer(int x, int y, int w, int h, boolean flipY) {
+    Gdx.gl.glPixelStorei(GL10.GL_PACK_ALIGNMENT, 1);
+
+    final Pixmap pixmap = new Pixmap(w, h, Pixmap.Format.RGBA8888);
+    ByteBuffer pixels = pixmap.getPixels();
+    Gdx.gl.glReadPixels(x, y, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, pixels);
+
+    final int numBytes = w * h * 4;
+    byte[] lines = new byte[numBytes];
+    if (flipY) {
+      final int numBytesPerLine = w * 4;
+      for (int i = 0; i < h; i++) {
+        pixels.position((h - i - 1) * numBytesPerLine);
+        pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
+      }
+      pixels.clear();
+      pixels.put(lines);
+    } else {
+      pixels.clear();
+      pixels.get(lines);
+    }
+
+    return pixmap;
+  }
+}
