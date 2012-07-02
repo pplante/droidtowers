2ff8882fe3dafc3561428733d8ed53f1c9ab3a16
Phil Plante <unhappyrobot@gmail.com>
2012-07-02 19:12:22 +0200

    Artifacts from release-v0.99.8

--- happydroids-common/src/com/happydroids/HappyDroidConsts.java
+++ happydroids-common/src/com/happydroids/HappyDroidConsts.java
@@ -5,13 +5,13 @@
 package com.happydroids;
 
 public class HappyDroidConsts {
-  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
+  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
   public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
   public static final String HAPPYDROIDS_API_KEY = "f88c02844c913c20f84ea29cbabec97c10c828a8caf204de41";
-  public static final boolean DEBUG = true;
+  public static final boolean DEBUG = false;
   public static boolean DISPLAY_DEBUG_INFO = true;
-  public static String VERSION = "v0.99.7";
-  public static String GIT_SHA = "0ddfd8fd51ef2d291038fc6d578d563d5adbcab0";
+  public static String VERSION = "v0.99.8";
+  public static String GIT_SHA = "1a5db39b9fc6e7489daf071cc0eff425988a6728";
   public static final byte[] OBFUSCATION_SALT = "ad076e981c2ea4103f1a6e30b5e8d0bd81bca536".getBytes();
   public static final String OBFUSCATION_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr1epMa3vopbqUJAfVe90GqfjfYUQB7Edb5fBUfTyLJ6lXQORZyvpiF+vTtCA0FEHI4jB9V4TMaJcFrnTk5MZDUHi1zkj0cSn9OG7znzEvSFwfJ63b/UWBZIdgx5/bE63Mkv3LL87aNFWlg5TzgR7mQtIxHjP4iP0y4mxJJujt49ArFxYWoIIBZCv0e5zyUtQDLPYfirp3nNUPLg/wW1VNeUutkR+71r6+z/a1MeKMfUzVOoSJisnNhqWhlSkrN4Mlz5ehJhDt/ubf9n0AFafusGnmrdYFwGrOjpWDCkOpLEvkvlZiNV+sshRVaRUCwFKPBbjV/NFsDKlkdgZnms2WwIDAQAB";
 }
