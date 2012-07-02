1ecf32058e79b50a71703b7325df6ae78b3b06e7
Phil Plante <unhappyrobot@gmail.com>
2012-07-02 08:02:36 +0200

    Revert "fixed the encryption issue with windows."
    This reverts commit 1e5b200ed0b6758df64099f0008cd7ee038af8b0.

--- happydroids-common/src/com/happydroids/security/AESObfuscator.java
+++ happydroids-common/src/com/happydroids/security/AESObfuscator.java
@@ -6,7 +6,6 @@ package com.happydroids.security;
 
 import com.happydroids.utils.Base64;
 import com.happydroids.utils.Base64DecoderException;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
@@ -14,7 +13,6 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
-import java.security.Security;
 import java.security.spec.KeySpec;
 
 /**
@@ -23,8 +21,9 @@ import java.security.spec.KeySpec;
 public class AESObfuscator {
   private static final String UTF8 = "UTF-8";
   private static final String KEYGEN_ALGORITHM = "PBKDF2WithHmacSHA1";
-  private static final String CIPHER_ALGORITHM = "AES/CTR/NoPadding";
-  private static final byte[] IV = {16, 74, 71, -80, 32, 101, -47, 72, 117, -14, 0, -29, 70, 65, -12, 74};
+  private static final String CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+  private static final byte[] IV =
+          {16, 74, 71, -80, 32, 101, -47, 72, 117, -14, 0, -29, 70, 65, -12, 74};
   private static final String header = AESObfuscator.class.getName() + "|";
 
   private Cipher mEncryptor;
@@ -32,14 +31,14 @@ public class AESObfuscator {
 
   public AESObfuscator(byte[] salt, String password) {
     try {
-      Security.addProvider(new BouncyCastleProvider());
       SecretKeyFactory factory = SecretKeyFactory.getInstance(KEYGEN_ALGORITHM);
-      KeySpec keySpec = new PBEKeySpec(password.toCharArray(), salt, 1024, 128);
+      KeySpec keySpec =
+              new PBEKeySpec(password.toCharArray(), salt, 1024, 256);
       SecretKey tmp = factory.generateSecret(keySpec);
       SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
-      mEncryptor = Cipher.getInstance(CIPHER_ALGORITHM, "BC");
+      mEncryptor = Cipher.getInstance(CIPHER_ALGORITHM);
       mEncryptor.init(Cipher.ENCRYPT_MODE, secret, new IvParameterSpec(IV));
-      mDecryptor = Cipher.getInstance(CIPHER_ALGORITHM, "BC");
+      mDecryptor = Cipher.getInstance(CIPHER_ALGORITHM);
       mDecryptor.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(IV));
     } catch (GeneralSecurityException e) {
       // This can't happen on a compatible Android device.
