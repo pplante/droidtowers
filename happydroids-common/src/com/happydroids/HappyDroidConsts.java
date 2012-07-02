1e5b200ed0b6758df64099f0008cd7ee038af8b0
Phil Plante <unhappyrobot@gmail.com>
2012-07-02 06:28:59 +0200

    fixed the encryption issue with windows.

--- happydroids-common/src/com/happydroids/HappyDroidConsts.java
+++ happydroids-common/src/com/happydroids/HappyDroidConsts.java
@@ -5,10 +5,10 @@
 package com.happydroids;
 
 public class HappyDroidConsts {
-  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
-  public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
+  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
+  public static final String HAPPYDROIDS_URI = "http://" + HAPPYDROIDS_SERVER;
   public static final String HAPPYDROIDS_API_KEY = "f88c02844c913c20f84ea29cbabec97c10c828a8caf204de41";
-  public static final boolean DEBUG = false;
+  public static final boolean DEBUG = true;
   public static boolean DISPLAY_DEBUG_INFO = true;
   public static String VERSION = "v0.99.6";
   public static String GIT_SHA = "c57eff53b53ab0bc669725c23a7b0dd02b642649";
