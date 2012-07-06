6d25b12ec35427f4d7eea9ede61a12d1ee28b938
Phil Plante <unhappyrobot@gmail.com>
2012-07-06 10:40:04 +0200

    broken html5 target

--- pull-libgdx-stable.sh
+++ pull-libgdx-stable.sh
@@ -1,27 +1,20 @@
-#! /bin/bash
+#!/bin/sh
 
-STABLE_NAME="libgdx-0.9.2"
-STABLE_URL="http://libgdx.googlecode.com/files/${STABLE_NAME}.zip"
-DOWNLOAD_PATH="/tmp/${STABLE_NAME}.zip"
-EXTRACT_PATH="/tmp/${STABLE_NAME}"
+TODAYS_DATE=`date -u +%C%y%m%d`
+NIGHTLY_NAME="libgdx-0.9.6"
+NIGHTLY_URL="http://libgdx.googlecode.com/files/${NIGHTLY_NAME}.zip"
+DOWNLOAD_PATH="/tmp/${NIGHTLY_NAME}.zip"
+EXTRACT_PATH="/tmp/${NIGHTLY_NAME}"
 
 PROJECT_DIR=`pwd`
 
-echo "* Downloading latest STABLE from:"
-echo 	$STABLE_URL
+echo "* Downloading latest nightly from:"
+echo 	$NIGHTLY_URL
 echo
 
-curl -# -o $DOWNLOAD_PATH $STABLE_URL
-DOWNLOAD_SUCCESS=$?
-
-if [[ $DOWNLOAD_SUCCESS != 0 ]]; then
-	echo
-	echo "Download failed, aborting."
-	exit $DOWNLOAD_SUCCESS
-fi;
 
 echo
-echo "* Unziping STABLE zip: ${DOWNLOAD_PATH}"
+echo "* Unziping nightly zip: ${DOWNLOAD_PATH}"
 echo
 
 unzip -qqo $DOWNLOAD_PATH -d $EXTRACT_PATH
@@ -37,20 +30,20 @@ echo
 echo "* Copying android libs..."
 echo
 
-ANDROID_LIBS_PATH="${PROJECT_DIR}/android/libs/."
+ANDROID_LIBS_PATH="${PROJECT_DIR}/android-shared/libs/."
 cp -R "${EXTRACT_PATH}/armeabi" $ANDROID_LIBS_PATH
 cp -R "${EXTRACT_PATH}/armeabi-v7a" $ANDROID_LIBS_PATH
 cp "${EXTRACT_PATH}/gdx-backend-android.jar" $ANDROID_LIBS_PATH
-cp "${EXTRACT_PATH}/gdx-backend-android-sources.jar" $ANDROID_LIBS_PATH
+cp "${EXTRACT_PATH}/sources/gdx-backend-android-sources.jar" $ANDROID_LIBS_PATH
 
 echo
 echo "* Copying desktop libs..."
 echo
 
 DESKTOP_LIBS_PATH="${PROJECT_DIR}/desktop/libs"
-cp "${EXTRACT_PATH}/gdx-openal-sources.jar" "${DESKTOP_LIBS_PATH}/sources"
+cp "${EXTRACT_PATH}/sources/gdx-openal-sources.jar" "${DESKTOP_LIBS_PATH}/sources"
 cp "${EXTRACT_PATH}/gdx-backend-lwjgl-natives.jar" "${DESKTOP_LIBS_PATH}/main"
-cp "${EXTRACT_PATH}/gdx-backend-lwjgl-sources.jar" "${DESKTOP_LIBS_PATH}/sources"
+cp "${EXTRACT_PATH}/sources/gdx-backend-lwjgl-sources.jar" "${DESKTOP_LIBS_PATH}/sources"
 cp "${EXTRACT_PATH}/gdx-backend-lwjgl.jar" "${DESKTOP_LIBS_PATH}/main"
 cp "${EXTRACT_PATH}/gdx-natives.jar" "${DESKTOP_LIBS_PATH}/main"
 
@@ -59,7 +52,7 @@ echo "* Copying shared libs..."
 echo
 
 SHARED_LIBS_PATH="${PROJECT_DIR}/main/libs/"
-cp "${EXTRACT_PATH}/gdx-sources.jar" "${SHARED_LIBS_PATH}/sources"
+cp "${EXTRACT_PATH}/sources/gdx-sources.jar" "${SHARED_LIBS_PATH}/sources"
 cp "${EXTRACT_PATH}/gdx.jar" "${SHARED_LIBS_PATH}/main"
 
 echo
