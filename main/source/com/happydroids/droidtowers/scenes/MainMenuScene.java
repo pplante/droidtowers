7d8638b4f52e052625a35e773007c0b327d2e9f2
Phil Plante <unhappyrobot@gmail.com>
2012-07-11 08:45:57 +0200

    Artifacts from release-1.0.14

--- main/source/com/happydroids/droidtowers/scenes/MainMenuScene.java
+++ main/source/com/happydroids/droidtowers/scenes/MainMenuScene.java
@@ -66,7 +66,7 @@ public class MainMenuScene extends SplashScene {
 
       buildMenuComponents();
 //      DebugUtils.loadFirstGameFound();
-      DebugUtils.createNonSavableGame(true);
+//      DebugUtils.createNonSavableGame(true);
 //      new PurchaseDroidTowersUnlimitedPrompt().show();
     }
   }
