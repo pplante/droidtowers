62c73d46a318d774e5cf698429a5f5a93c71a254
Phil Plante <unhappyrobot@gmail.com>
2012-11-25 21:59:15 -0800

    Artifacts from release-1.1.16

--- main/source/com/happydroids/HappyDroidConsts.java
+++ main/source/com/happydroids/HappyDroidConsts.java
@@ -5,15 +5,15 @@
 package com.happydroids;
 
 public class HappyDroidConsts {
-  public static final String HAPPYDROIDS_SERVER = "local.happydroids.com";
-  public static final String HAPPYDROIDS_URI = "http://" + HAPPYDROIDS_SERVER;
+  public static final String HAPPYDROIDS_SERVER = "www.happydroids.com";
+  public static final String HAPPYDROIDS_URI = "https://" + HAPPYDROIDS_SERVER;
   public static final String HAPPYDROIDS_API_KEY = "f88c02844c913c20f84ea29cbabec97c10c828a8caf204de41";
-  public static final boolean DEBUG = true;
+  public static final boolean DEBUG = false;
   public static boolean DISPLAY_DEBUG_INFO = true;
   public static final long HAPPYDROIDS_PING_FREQUENCY = 180000;
-  public static String VERSION = "1.1.14";
-  public static int VERSION_CODE = 1114;
-  public static String GIT_SHA = "07585f8b9094ab086ccadc8d542feef18f9f8afa";
+  public static String VERSION = "1.1.16";
+  public static int VERSION_CODE = 1116;
+  public static String GIT_SHA = "b3b5e0dd7167b4d94785b79f6a59edebff335131";
   public static final byte[] OBFUSCATION_SALT = "ad076e981c2ea4103f1a6e30b5e8d0bd81bca536".getBytes();
   public static final String OBFUSCATION_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr1epMa3vopbqUJAfVe90GqfjfYUQB7Edb5fBUfTyLJ6lXQORZyvpiF+vTtCA0FEHI4jB9V4TMaJcFrnTk5MZDUHi1zkj0cSn9OG7znzEvSFwfJ63b/UWBZIdgx5/bE63Mkv3LL87aNFWlg5TzgR7mQtIxHjP4iP0y4mxJJujt49ArFxYWoIIBZCv0e5zyUtQDLPYfirp3nNUPLg/wW1VNeUutkR+71r6+z/a1MeKMfUzVOoSJisnNhqWhlSkrN4Mlz5ehJhDt/ubf9n0AFafusGnmrdYFwGrOjpWDCkOpLEvkvlZiNV+sshRVaRUCwFKPBbjV/NFsDKlkdgZnms2WwIDAQAB";
   public static final String SENTRY_DSN = "http://5a007db6832348879a35ab0ccd4db39b:36c43e145c4a40a4ba4a8b9dda8d837a@www.crashdroid.com/4";
