f71611dd9f469befca9171c7cdd52cd4b37d5c2a
Phil Plante <unhappyrobot@gmail.com>
2013-12-25 12:12:30 -0800

    fix the libgdx script

--- pull-libgdx-stable.sh
+++ pull-libgdx-stable.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 TODAYS_DATE=`date -u +%C%y%m%d`
-NIGHTLY_NAME="libgdx-0.9.6"
-NIGHTLY_URL="http://libgdx.googlecode.com/files/${NIGHTLY_NAME}.zip"
+NIGHTLY_NAME="libgdx-0.9.9"
+NIGHTLY_URL="http://libgdx.badlogicgames.com/releases/${NIGHTLY_NAME}.zip"
 DOWNLOAD_PATH="/tmp/${NIGHTLY_NAME}.zip"
 EXTRACT_PATH="/tmp/${NIGHTLY_NAME}"
 
 PROJECT_DIR=`pwd`
 
 echo "* Downloading latest nightly from:"
-echo 	$NIGHTLY_URL
+echo $NIGHTLY_URL
 echo
 
+curl -# -o $DOWNLOAD_PATH $NIGHTLY_URL
+DOWNLOAD_SUCCESS=$?
+
+if [[ $DOWNLOAD_SUCCESS != 0 ]]; then
+	echo
+	echo "Download failed, aborting."
+	exit $DOWNLOAD_SUCCESS
+fi;
 
 echo
 echo "* Unziping nightly zip: ${DOWNLOAD_PATH}"
